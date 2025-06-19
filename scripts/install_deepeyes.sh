3959ef34eca9b2fd582e078b77d5d9356c516c61
yingzhepeng <yingzhepeng@tencent.com>
2025-06-19 11:41:23 +0800

    use uv

--- scripts/install_deepeyes.sh
+++ scripts/install_deepeyes.sh
@@ -1,10 +1,12 @@
-pip install evaluate
-pip install vllm==0.8.2
-pip install -U pynvml
-pip install mathruler
-pip install pydantic --upgrade
-pip install openai --upgrade
-pip install tensordict==0.6.2
-pip install triton==3.1.0
-pip install qwen_vl_utils
-pip install math_verify
+uv pip install evaluate
+uv pip install vllm==0.8.2
+uv pip install -U pynvml
+uv pip install mathruler
+uv pip install pydantic --upgrade
+uv pip install openai --upgrade
+uv pip install tensordict==0.6.2
+uv pip install triton==3.1.0
+uv pip install qwen_vl_utils
+uv pip install math_verify
+uv pip install flash-attn==2.7.4.post1 --no-build-isolation
+uv pip install transformers==4.51.0
